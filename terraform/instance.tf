bc852df6361cc6ca0409171d16dfed4f1cc9a372
Antonio Thomacelli <tonnytg@gmail.com>
2023-12-25 22:19:33 -0300

    feat: change instance config to ubuntu 2204

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,24 +1,57 @@
-# Create a single Compute Engine instance
-resource "google_compute_instance" "default" {
-  name         = "micro-vm"
-  machine_type = "f1-micro"
-  zone         = "southamerica-east1-a"
-  tags         = ["ssh", "http"]
+# Este código é compatível com a versão 4.25.0 do Terraform e com as que têm compatibilidade com versões anteriores à 4.25.0.
+# Para informações sobre como validar esse código do Terraform, consulte https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
 
+resource "google_compute_instance" "instance-1" {
   boot_disk {
+    auto_delete = true
+    device_name = var.name_instance
+
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 10
+      type  = "pd-standard"
     }
+
+    mode = "READ_WRITE"
   }
 
-  # Install Flask
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "f1-micro"
+  name         = var.name_instance
 
   network_interface {
-    subnetwork = google_compute_subnetwork.default.id
-
     access_config {
-      # Include this section to give the VM an external IP address
+      network_tier = "PREMIUM"
     }
+
+    subnetwork = "projects/${var.project}/regions/${var.region}/subnetworks/default"
   }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = "southamerica-east1-a"
 }
